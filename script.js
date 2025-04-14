32534de89410d47cebb50ee325860c366824a38d
Cynthia-mbesa <cynthiambesa@gmail.com>
2025-04-14 11:54:19 +0300

    changes

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
     alert('Thank you for your message! We will get back to you shortly.');
     this.reset();
 });
+
+const cards = document.querySelectorAll('.service-card');
+
+let flippedCard = null;
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    if (flippedCard && flippedCard !== card) {
+      // Unflip the previously flipped card
+      flippedCard.querySelector('.card-inner').style.transform = 'rotateY(0deg)';
+    }
+
+    const cardInner = card.querySelector('.card-inner');
+
+    if (flippedCard === card) {
+      // If clicking the same card, unflip it
+      cardInner.style.transform = 'rotateY(0deg)';
+      flippedCard = null;
+    } else {
+      // Flip the clicked card
+      cardInner.style.transform = 'rotateY(180deg)';
+      flippedCard = card;
+    }
+  });
+});
